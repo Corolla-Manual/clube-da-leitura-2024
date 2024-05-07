e2cfe3a31cef2064ac6d58478e0327fcceb0e654
Eduardo <eduardotortellidudu9@uniplaclages.edu.br>
2024-05-07 17:02:26 -0300

    Mais Implementação

--- ClubeDaLeitura.ConsoleApp/ModuloAmigo/Amigo.cs
+++ ClubeDaLeitura.ConsoleApp/ModuloAmigo/Amigo.cs
@@ -1,6 +1,7 @@
 ﻿using ClubeDaLeitura.ConsoleApp.Compartilhado;
 using ClubeDaLeitura.ConsoleApp.ModuloMulta;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,13 +17,41 @@ namespace ClubeDaLeitura.ConsoleApp.ModuloAmigo
         public string Endereco { get; set; }
         public Multa Multa { get; set; }
 
-        public Amigo(string nome, string nomeresponsavel, string telefone, string endereco, Multa multa)
+        public Amigo(string nome, string nomeresponsavel, string telefone, string endereco)
         {
             Nome = nome;
             NomeResponsavel = nomeresponsavel;
             Telefone = telefone;
             Endereco = endereco;
-            Multa = multa;
+        }
+
+        public override ArrayList Validar()
+        {
+            ArrayList erros = new ArrayList();
+
+            if (string.IsNullOrEmpty(Nome.Trim()))
+                erros.Add("O campo \"nome\" é obrigatório");
+
+            if (string.IsNullOrEmpty(NomeResponsavel.Trim()))
+                erros.Add("O campo \"nome do responsável\" é obrigatório");
+
+            if (string.IsNullOrEmpty(Telefone.Trim()))
+                erros.Add("O campo \"Telefone\" é obrigatório");
+
+            if (string.IsNullOrEmpty(Endereco.Trim()))
+                erros.Add("O campo \"Endereço\" é obrigatório");
+
+            return erros;
+        }
+
+        public override void AtualizarRegistro(EntidadeBase novoRegistro)
+        {
+            Amigo novasInformacoes = (Amigo)novoRegistro;
+
+            this.Nome = novasInformacoes.Nome;
+            this.NomeResponsavel = novasInformacoes.NomeResponsavel;
+            this.Telefone = novasInformacoes.Telefone;
+            this.Endereco = novasInformacoes.Endereco;
         }
     }
 }
