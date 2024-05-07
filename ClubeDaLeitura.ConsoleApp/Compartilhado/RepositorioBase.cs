163360576420ae60950b88b07dd833306af8a7b4
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-05-07 16:31:25 -0300

    Base Compartilhada concluida

--- ClubeDaLeitura.ConsoleApp/Compartilhado/RepositorioBase.cs
+++ ClubeDaLeitura.ConsoleApp/Compartilhado/RepositorioBase.cs
@@ -1,12 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
 
 namespace ClubeDaLeitura.ConsoleApp.Compartilhado
 {
-    internal abstract class RepositorioBas
+    internal abstract class RepositorioBase
     {
+        protected ArrayList registros = new();
+
+        protected int contadorId = 1;
+
+        public void Cadastrar(EntidadeBase novoRegistro)
+        {
+            novoRegistro.Id = contadorId++;
+
+            registros.Add(novoRegistro);
+        }
+
+        public bool Editar(int id, EntidadeBase novaEntidade)
+        {
+            novaEntidade.Id = id;
+
+            foreach (EntidadeBase entidade in registros)
+            {
+                if (entidade == null)
+                    continue;
+
+                else if (entidade.Id == id)
+                {
+                    entidade.AtualizarRegistro(novaEntidade);
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool Excluir(int id)
+        {
+            foreach (EntidadeBase entidade in registros)
+            {
+                if (entidade == null)
+                    continue;
+
+                else if (entidade.Id == id)
+                {
+                    registros.Remove(entidade);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public ArrayList SelecionarTodos()
+        {
+            return registros;
+        }
+
+        public EntidadeBase SelecionarPorId(int id)
+        {
+            foreach (EntidadeBase entidade in registros)
+            {
+
+                if (entidade == null)
+                    continue;
+
+                else if (entidade.Id == id)
+                    return entidade;
+            }
+
+            return null;
+        }
+
+        public bool Existe(int id)
+        {
+            foreach (EntidadeBase entidade in registros)
+            {
+                if (entidade == null)
+                    continue;
+
+                else if (entidade.Id == id)
+                    return true;
+            }
+
+            return false;
+        }
     }
 }
---
+++ ClubeDaLeitura.ConsoleApp/Compartilhado/TelaPrincipal.cs
@@ -0,0 +1,29 @@
+﻿namespace ClubeDaLeitura.ConsoleApp.Compartilhado
+{
+    internal class TelaPrincipal
+    {
+        public static char ApresentarMenuPrincipal()
+        {
+            Console.Clear();
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("|           Clube de Leitura           |");
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine();
+
+            Console.WriteLine("1 - Cadastro de ");
+
+
+            Console.WriteLine("S - Sair");
+
+            Console.WriteLine();
+
+            Console.Write("Escolha uma das opções: ");
+
+            char opcaoEscolhida = Console.ReadLine()[0];
+
+            return opcaoEscolhida;
+        }
+    }
+}
